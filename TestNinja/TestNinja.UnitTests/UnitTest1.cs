87f32d592775cfd3c29ce4166209fd2c2a191faf
John Rufino <jbrufino@pjlhuillier.com>
2023-03-17 14:02:43 +0800

    first test

---
+++ TestNinja/TestNinja.UnitTests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        // standard for naming test methods 
+        // method_scenario_expectedbehaviors
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            // trip A convention
+            // arrange
+            var reservation = new Reservation();
+
+            // act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            // assert
+            Assert.IsTrue(result);
+        }
+    }
+}
